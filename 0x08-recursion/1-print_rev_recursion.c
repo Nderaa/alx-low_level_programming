04653df7d071f9a62f73a063b66a6dae8b30b9b1
Nderaa <haikashammah@gmail.com>
2023-07-31 13:24:23 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,22 @@
 #include "main.h"
-/**
- * _print_rev_recursion - print string in a reversed manner
- *
- * @s: the string to be reversed
- *
- * Return: void
- */
+#include <unistd.h>
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
-	{
+	if (*s == '\0')
+	return;
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
-	}
+}
+
+int main(void)
+{
+	char str[] = "\nColton Walker";
+	_print_rev_recursion(str);
+
+	return (0);
+
 }
